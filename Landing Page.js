2da38e47e498504576ad7f469456095c21e3ac75
HakamRaza <ismatcd@yahoo.com>
2020-06-24 08:53:59 +0800

    on progress, figuring basic layout

---
+++ Landing Page.js
@@ -0,0 +1,16 @@
+
+// detect window screen and height to arrange position
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+
+
+function centralisedOrigin () {
+    //must same as CSS
+    var originWidth = 1000;
+    var originHeight = 1000;
+    
+    document.getElementById('zoomOrigin').style.top = `${windowHeight/2 - originHeight/2}px`;
+    
+    document.getElementById('zoomOrigin').style.left = `${windowWidth/2 - originWidth/2}px`;
+
+}
